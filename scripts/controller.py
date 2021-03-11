852c9196fd82eb9ff0fb3e23ac942a8629307773
linnan1 <linnan1@foxmail.com>
2021-03-11 11:21:44 +0800

    rename traj_server.py: controller.py

--- scripts/controller.py
+++ scripts/controller.py
@@ -7,10 +7,10 @@ class TrajServer:
         self.planning_cmd_sub = rospy.Subscriber('/planning/pos_cmd',PositionCommand, self.call_back)
         self.cmd_handle = laikago_command_handle()
     def call_back(self,pos_cmd):
-        self.cmd_handle.set_speed(pos_cmd.velocity.x,pos_cmd.yaw_dot,pos_cmd.velocity.y)
+        self.cmd_handle.set_speed(pos_cmd.velocity.x,-pos_cmd.yaw_dot,pos_cmd.velocity.y)
         self.cmd_handle.send()
 if __name__ == '__main__':
-    rospy.init_node('laikago_traj_server', anonymous=False)
+    rospy.init_node('laikago_controller', anonymous=False)
     traj_server = TrajServer()
     try:
         rospy.spin()
